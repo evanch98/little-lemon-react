64e6d05ce72fc18337c52cab81e4d97b8df25a1b
Kyaw Thu <evanch98@gmail.com>
2023-09-30 16:20:15 +0800

    add a new component Header

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+interface HeaderProps {
+  title: string;
+}
+
+const Header = ({ title }: HeaderProps) => {
+  return <h1 className="header text-[#333333]">{title}</h1>;
+};
+
+export default Header;
