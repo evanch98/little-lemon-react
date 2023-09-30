5f4891a81a7bd4a4e457ddaf1caa79225561cd21
Kyaw Thu <evanch98@gmail.com>
2023-09-30 16:32:47 +0800

    update the Nav component

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,40 @@
+import { Container } from "./common/container";
+
+const links = [
+  {
+    name: "Home",
+  },
+  {
+    name: "About",
+  },
+  {
+    name: "Menu",
+  },
+  {
+    name: "Reservations",
+  },
+  {
+    name: "Order Online",
+  },
+];
+
 const Nav = () => {
-  return <nav></nav>;
+  return (
+    <Container className="py-[32px]">
+      <nav className="flex items-center justify-between">
+        <img
+          src="/assets/Logo.svg"
+          alt="Little Lemon Logo"
+          className="w-[300px] h-[81.1px]"
+        />
+        <ul className="flex items-center justify-center gap-x-[16px]">
+          {links.map((link) => (
+            <li key={link.name}>{link.name}</li>
+          ))}
+        </ul>
+      </nav>
+    </Container>
+  );
 };
 
 export default Nav;
