714341d03619a7654356d3c88afdca672a745e26
Kyaw Thu <evanch98@gmail.com>
2023-09-30 16:15:42 +0800

    add a new component Container

---
+++ src/components/common/container.tsx
@@ -0,0 +1,21 @@
+import { cn } from "../../lib/utils";
+
+interface ContainerProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export const Container = ({ className, children }: ContainerProps) => {
+  return (
+    <div className="w-full flex flex-col items-center justify-center">
+      <div
+        className={cn(
+          "w-full max-w-[1440px] flex flex-col px-[120px]",
+          className
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
