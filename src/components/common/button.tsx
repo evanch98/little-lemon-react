97e279c72ec952687e70115235773d8f2f1dd843
Kyaw Thu <evanch98@gmail.com>
2023-09-30 16:49:02 +0800

    update the Button component

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -3,7 +3,11 @@ interface ButtonProps {
 }
 
 const Button = ({ children }: ButtonProps) => {
-  return <button>{children}</button>;
+  return (
+    <button className="flex items-center justify-center py-[16px] px-[32px] bg-[#F4CE14] text-[#333333] rounded-[16px]">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
