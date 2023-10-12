995d6831515355425f4a9042baa562c8dff388b9
Kyaw Thu <evanch98@gmail.com>
2023-10-12 19:25:26 +0800

    update the specials.tsx

--- src/components/section/speicals.tsx
+++ src/components/section/speicals.tsx
@@ -1,6 +1,27 @@
 import Button from "../common/button";
 import MenuCard from "../common/menu-card";
 
+const speicals = [
+  {
+    name: "Greek Salad",
+    price: "$12.99",
+    description:
+      "The famous Greek salad of crispy lettuce, peppers, olives, and our Chicago feta cheese, garnished with crunchy garlic and rosemary croutons.",
+  },
+  {
+    name: "Bruschetta",
+    price: "$5.99",
+    description:
+      "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+  },
+  {
+    name: "Lemon Desert",
+    price: "$4.99",
+    description:
+      "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+  },
+];
+
 const Specials = () => {
   return (
     <div className="flex flex-col space-y-[48px] items-center justify-center w-full text-[#333333]">
