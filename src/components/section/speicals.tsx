38aabeccd17757239c77a98341a28b9c3a87b75a
Kyaw Thu <evanch98@gmail.com>
2023-10-12 18:59:50 +0800

    update the Specials component

--- src/components/section/speicals.tsx
+++ src/components/section/speicals.tsx
@@ -1,5 +1,16 @@
+import Button from "../common/button";
+
 const Specials = () => {
-  return <div>Specials</div>;
+  return (
+    <div className="flex flex-col space-y-[48px] items-center justify-center w-full text-[#333333]">
+      <div className="flex items-center justify-between md:w-[900px] w-full">
+        <h1 className="text-[4rem] markazi-text">This week's specials</h1>
+        <Button>
+          <p className="font-bold karla text-lg">Online Menu</p>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Specials;
