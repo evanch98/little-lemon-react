90f81cab820c01afeb8950982fa9ca4e4434e0ee
Kyaw Thu <evanch98@gmail.com>
2023-10-12 18:54:08 +0800

    add the Specials component

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,11 +1,16 @@
 import { Container } from "./common/container";
 import Hero from "./section/hero";
+import Specials from "./section/speicals";
 
 const Main = () => {
   return (
     <>
       <Hero />
-      <Container>Hello</Container>
+      <Container>
+        <div className="pt-[128px] flex flex-col space-y-[128px] w-full">
+          <Specials />
+        </div>
+      </Container>
     </>
   );
 };
