6fb17ce22d0559d0aa4966a95dd6813b11d6c573
Kyaw Thu <evanch98@gmail.com>
2023-10-12 18:06:02 +0800

    add the Hero component

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,12 @@
+import { Container } from "./common/container";
+import Hero from "./section/hero";
+
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <Container>
+      <Hero />
+    </Container>
+  );
 };
 
 export default Main;
