2878976f991d2b0bc579b9afefe27b744531a08e
Kyaw Thu <evanch98@gmail.com>
2023-10-12 18:25:21 +0800

    update the Hero component

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -1,9 +1,25 @@
+import Button from "../common/button";
+
 const Hero = () => {
   return (
-    <div className="bg-[#495E57] text-[#EDEFEE] flex items-center justify-center py-[32px]">
-      <div className="w-full flex flex-col items-center justify-center gap-y-[24px]">
-        <div className="w-full flex flex-col items-center justify-center gap-y-[16px]">
-          <h1 className="text-[#F4CE14]">Little Lemon</h1>
+    <div className="bg-[#495E57] text-[#EDEFEE] flex items-center justify-center py-[32px] px-[146px]">
+      <div className="w-full flex flex-col items-center justify-center space-y-[24px]">
+        <div className="w-full flex flex-col items-start justify-center -space-y-[20px]">
+          <h1 className="text-[#F4CE14] markazi-text text-[4rem]">
+            Little Lemon
+          </h1>
+          <p className="text-6xl markazi-text">Chicago</p>
+        </div>
+        <div className="w-full flex flex-col items-start justify-center space-y-[16px]">
+          <p className="karla text-lg">
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <Button>
+            <p className="text-[#333333] font-bold karla text-lg">
+              Reserve a table
+            </p>
+          </Button>
         </div>
       </div>
     </div>
