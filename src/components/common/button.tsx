963b292a94aa49f07ce4a8afac44fe81ae9d2850
Kyaw Thu <evanch98@gmail.com>
2023-10-12 18:41:46 +0800

    update the Button component

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 
 const Button = ({ children }: ButtonProps) => {
   return (
-    <button className="flex items-center justify-center py-[16px] px-[32px] bg-[#F4CE14] text-[#333333] rounded-[16px]">
+    <button className="flex items-center justify-center py-[16px] px-[32px] bg-[#F4CE14] text-[#333333] rounded-[16px] hover:opacity-75 transition">
       {children}
     </button>
   );
