240dc9ac684c36d00196b6d48b95029f0ca886dc
Kyaw Thu <evanch98@gmail.com>
2023-10-12 19:26:36 +0800

    update the MenuCard

--- src/components/common/menu-card.tsx
+++ src/components/common/menu-card.tsx
@@ -1,19 +1,21 @@
 import Button from "./button";
 
-const MenuCard = () => {
+interface MenuCardProps {
+  name: string;
+  price: string;
+  description: string;
+}
+
+const MenuCard = ({ name, price, description }: MenuCardProps) => {
   return (
     <div className="flex flex-col bg-[#EDEFEE]">
       <img src="/assets/greek salad.jpg" height={274} width={424} alt="Dish" />
       <div className="w-full flex flex-col p-[24px] space-y-[32px] text-[#333333]">
         <div className="flex items-center justify-between">
-          <h2 className="font-bold karla text-lg">Greek Salad</h2>
-          <p className="font-bold karla text-lg text-[#EE9972]">$12.99</p>
+          <h2 className="font-bold karla text-lg">{name}</h2>
+          <p className="font-bold karla text-lg text-[#EE9972]">{price}</p>
         </div>
-        <p className="w-[375px] opacity-75 font-medium">
-          The famous Greek salad of crispy lettuce, peppers, olives, and our
-          Chicago feta cheese, garnished with crunchy garlic and rosemary
-          croutons.
-        </p>
+        <p className="w-[375px] opacity-75 font-medium">{description}</p>
         <Button>
           <p className="text-[#333333] font-bold karla text-lg">
             Order a Delivery
