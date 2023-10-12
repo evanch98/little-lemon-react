6caa7b028cb0214f00e39d07cc5bfd54b707901b
Kyaw Thu <evanch98@gmail.com>
2023-10-12 18:11:06 +0800

    update the Hero component

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -1,5 +1,13 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="bg-[#495E57] text-[#EDEFEE] flex items-center justify-center py-[32px]">
+      <div className="w-full flex flex-col items-center justify-center gap-y-[24px]">
+        <div className="w-full flex flex-col items-center justify-center gap-y-[16px]">
+          <h1 className="text-[#F4CE14]">Little Lemon</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
