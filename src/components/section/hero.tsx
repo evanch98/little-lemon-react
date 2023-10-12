5fb6c51b04f85ebd7eab1727b30a2c128071edd1
Kyaw Thu <evanch98@gmail.com>
2023-10-12 19:31:31 +0800

    update the Hero component

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -2,7 +2,7 @@ import Button from "../common/button";
 
 const Hero = () => {
   return (
-    <div className="bg-[#495E57] text-[#EDEFEE] flex items-center justify-between py-[32px] md:px-[266px] px-[16px]">
+    <div className="bg-[#495E57] text-[#EDEFEE] flex items-center justify-between md:justify-center md:space-x-[280px] py-[32px] md:px-[266px] px-[16px]">
       <div className="max-w-[328px] flex flex-col items-center justify-center space-y-[24px]">
         <div className="w-full flex flex-col items-start justify-center -space-y-[20px]">
           <h1 className="text-[#F4CE14] markazi-text text-[4rem]">
