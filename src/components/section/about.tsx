3e68e3593d732d827e06176b8a0107d41bc4ba7b
Kyaw Thu <evanch98@gmail.com>
2023-10-15 22:13:09 +0800

    update About

--- src/components/section/about.tsx
+++ src/components/section/about.tsx
@@ -1,8 +1,6 @@
-import Button from "../common/button";
-
 const About = () => {
   return (
-    <div className="bg-[#495E57] text-[#EDEFEE] flex items-center justify-between md:justify-center md:space-x-[280px] py-[32px] md:px-[266px] px-[16px]">
+    <div className="mt-[128px] bg-[#495E57] text-[#EDEFEE] flex items-center justify-between md:justify-center md:space-x-[280px] py-[32px] md:px-[266px] px-[16px]">
       <div className="max-w-[328px] flex flex-col items-center justify-center space-y-[24px]">
         <div className="w-full flex flex-col items-start justify-center -space-y-[20px]">
           <h1 className="text-[#F4CE14] markazi-text text-[4rem]">
@@ -15,19 +13,14 @@ const About = () => {
             We are a family owned Mediterranean restaurant, focused on
             traditional recipes served with a modern twist.
           </p>
-          <Button>
-            <p className="text-[#333333] font-bold karla text-lg">
-              Reserve a table
-            </p>
-          </Button>
         </div>
       </div>
       <div className="h-full rounded-2xl overflow-hidden">
         <img
-          src="assets/restauranfood.jpg"
+          src="assets/mario.jpg"
           alt="RestaurantImage"
-          width={200}
-          height={350}
+          width={400}
+          height={280}
         />
       </div>
     </div>
