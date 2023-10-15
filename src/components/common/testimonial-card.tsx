821f7973b2e6790aa491ace16bf73a0358439ee6
Kyaw Thu <evanch98@gmail.com>
2023-10-15 22:04:27 +0800

    update the TestimonialCard

--- src/components/common/testimonial-card.tsx
+++ src/components/common/testimonial-card.tsx
@@ -6,8 +6,8 @@ interface TestimonialCardProps {
 
 const TestimonialCard = ({ imgUrl, name, feedback }: TestimonialCardProps) => {
   return (
-    <div className="flex flex-col p-6 bg-[#EDEFEE] space-y-[24px]">
-      <div className="flex items-center justify-between w-full space-x-[32px]">
+    <div className="flex flex-col justify-start items-start p-6 bg-[#EDEFEE] space-y-[24px] text-[1.125rem]  karla">
+      <div className="flex items-center justify-center space-x-[32px]">
         <img
           src={imgUrl}
           width={100}
@@ -15,8 +15,9 @@ const TestimonialCard = ({ imgUrl, name, feedback }: TestimonialCardProps) => {
           className="rounded-full overflow-hidden"
           alt="Profile"
         />
-        <p className="text-[1.125rem] font-bold">{name}</p>
+        <p className="font-bold">{name}</p>
       </div>
+      <p className="text-[#333333] opacity-75 w-[265px]">{feedback}</p>
     </div>
   );
 };
